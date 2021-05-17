126db3fb09eaa6fa376312038c087fc41a2fd474
lesleysauter <64439597+lesleysauter@users.noreply.github.com>
2021-05-17 10:32:11 -0700

    added user and post models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Model
 
 # Create your models here.
+
+class User(Model):
+
+    username = models.CharField(max_length=25)
+    name = models.CharField(max_length=100)
+    password = models.CharField(max_length=250)
+    current_city = models.CharField(max_length=250)
+    join_date = models.DateTimeField(auto_now_add=True)
+
+
+class Post(Model):
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    location = models.CharField(max_length=250)
+    content = models.CharField(max_length=10000)
+
--- main_app/views.py
+++ main_app/views.py
@@ -7,4 +7,3 @@ from django.views.generic.base import TemplateView
 class Home(TemplateView):
     template_name = "home.html"
 
-      
