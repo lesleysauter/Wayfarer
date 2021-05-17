9af85835deed60d8866c3f5707ab71441bc68b02
wsushinsky <wyattsushi@gmail.com>
2021-05-17 16:08:14 -0700

    post now shows author
    Signed-off-by: wsushinsky <wyattsushi@gmail.com>

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('profile/', views.Profile.as_view(), name="profile"),
+    path('profile/', views.ShowProfile.as_view(), name="profile"),
     path('login/', views.Login.as_view(), name="customlogin"),
     path('signup/', views.Signup.as_view(), name="signup"),
-    path('post/<int:id>/', views.Post.as_view(), name="post"),
+    path('post/<int:pk>/', views.ShowPost.as_view(), name="post"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse, response
 from django.views.generic.base import TemplateView
+from django.views.generic import DetailView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post, Profile
+
 # Create your views here.
 
 class Home(TemplateView):
@@ -13,10 +16,9 @@ class Home(TemplateView):
         context = super().get_context_data(**kwargs)
         context["form"] = UserCreationForm()
         return context
-        
 
 
-class Profile(TemplateView):
+class ShowProfile(TemplateView):
     template_name = "profile.html"
 
 
@@ -31,7 +33,6 @@ class Login(View):
         
         else:
             return HttpResponse("Unable to login!", content_type="text/plain")
-            
 
 
 class Signup(View):
@@ -44,5 +45,9 @@ class Signup(View):
         else:
             return HttpResponse("Unable to create profile!", content_type="text/plain")
 
-class Post(TemplateView):
-    template_name = "show-post.html"
+
+class ShowPost(View):
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        context = {"post": post}
+        return render(request, "show-post.html", context)
