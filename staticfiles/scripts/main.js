6f80dcf324e6cf93864bdea1bbdb93a0bf56a8e6
Scott Perry <scottperry@Scotts-MacBook-Air.local>
2021-05-19 13:37:38 -0700

    heroku update

---
+++ staticfiles/scripts/main.js
@@ -0,0 +1,38 @@
+$(".auth-box-summoner").on("click", function() {
+    $(".auth-box").css("display", "initial");
+    $(".fade").css("display", "initial");
+})
+
+$(".fade").on("click", function() {
+    $(".auth-box").css("display", "none");
+    $(".fade").css("display", "none");
+})
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("sqSlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  
+}
